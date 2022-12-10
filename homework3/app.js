624166a9f784083303649779b55b60c80e013b7b
kristijan <jankuloski.k@gmail.com>
2022-12-10 15:10:59 +0100

    half of hw3

--- homework3/app.js
+++ homework3/app.js
@@ -1,22 +1,59 @@
-function sub(number1, number2){
-    if(Number.isNaN(number1) || Number.isNaN(number2)){
-        return "Invalid input";
+// Calculating birth year
+function calculateBirthYear(birthYear){
+    birthYear = Number(birthYear);
+    if(Number.isNaN(birthYear)){
+        return "Invalid year";
     }
-    return number1 - number2;
-}
-function mult(number1, number2){
-    if(Number.isNaN(number1) || Number.isNaN(number2)){
-        return "Invalid input";
+    let currentDate = new Date();
+    if(birthYear > currentDate.getFullYear()){
+        return "You've entered a year past our current year";
     }
-    return number1 * number2;
-}
-function div(number1, number2){
-    if(Number.isNaN(number1) || Number.isNaN(number2)){
-        return "Invalid input";
-    }
-    return number1 / number2;
+    return currentDate.getFullYear() - birthYear;
 }
 
-let input1 = Number(prompt("Enter 1 number"));
-let input2 = Number(prompt("Enter second number"));
-console.log(`Subtraction ${sub(input1, input2)}, Multiplication ${mult(input1, input2)}, Division ${div(input1, input2)}`)
+// Variable type and return
+function returnTypeAndPrint(variable){
+    console.log(typeof(variable));
+    return typeof(variable);
+}
+
+// Calculate dog years
+function calculateDogYears(years, dogOrHuman = "dog"){
+    years = Number(years);
+    if(Number.isNaN(years)){
+        return "Invalid years input";
+    }
+    dogOrHuman = dogOrHuman.toLowerCase();
+    if(years === 0){
+        return "0?";
+    }
+    if(dogOrHuman === "dog"){
+        if(years <= 1){
+            return years*15;
+        }
+        if(years <= 2){
+            return 15 + (years-1) * 9;
+        }
+        return 24 + (years - 2) * 5;
+    }
+    if(dogOrHuman === "human"){
+        if(years <= 15){
+            return years/15;
+        }
+        if(years <= 24){
+            return (years - 15) / 9 + 1;
+        }
+        return (years - 24) / 5 + 2;
+    }
+    return "You didn't enter a valid dog/human";
+}
+
+// Birth year test
+// const birthYearInput = prompt("Enter your birth year");
+// console.log(calculateBirthYear(birthYearInput));
+
+const nullObj = null;
+const numberObj = 12;
+const stringObj = "yes";
+const boolObj = true;
+let undefinedObj;
