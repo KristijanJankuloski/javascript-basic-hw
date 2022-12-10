55d4c7616e7d080d9cbf3afd2b574f60220210fa
Kristijan <jankuloski.k@gmail.com>
2022-12-10 01:24:32 +0100

    homework 2 finished

--- homework2/app.js
+++ homework2/app.js
@@ -1,17 +1,49 @@
-const userInput = Number(prompt("How much money you got in dollars?"));
+const yearOfBirth = Number(prompt("What is your year of birth?"));
 
-if(userInput <=0){
-    console.log("U broke my guy");
-} else if(userInput > 0 && userInput <= 500){
-    console.log("Buy a laptop or smt");
-} else if(userInput > 500 && userInput <= 10000){
-    console.log("Start thinking about a car");
-} else if(userInput > 10000 && userInput <= 100000){
-    console.log("Think about a house");
-} else if(userInput > 100000 && userInput <=100000000000){
-    console.log("u rich");
-} else if(userInput > 100000000000){
-    console.log("Jeff Bezos?");
-}else{
-    console.log("is this even a number");
+function calculateZodiacSign(year){
+    if(Number.isNaN(year)){
+        return "Invalid input";
+    }
+
+    const sign = (year - 4) % 12;
+    if(sign === 0){
+        return "Rat";
+    }
+    if(sign === 1){
+        return "Ox";
+    }
+    if(sign === 2){
+        return "Tiger";
+    }
+    if(sign === 3){
+        return "Rabbit";
+    }
+    if(sign === 4){
+        return "Dragon";
+    }
+    if(sign === 5){
+        return "Snake";
+    }
+    if(sign === 6){
+        return "Horse";
+    }
+    if(sign === 7){
+        return "Goat";
+    }
+    if(sign === 8){
+        return "Monkey";
+    }
+    if(sign === 9){
+        return "Rooster";
+    }
+    if(sign === 10){
+        return "Dog";
+    }
+    if(sign === 11){
+        return "Pig";
+    }
+
+    return null;
 }
+
+console.log(`Your Chinease zodiac sign is ${calculateZodiacSign(yearOfBirth)}`);
