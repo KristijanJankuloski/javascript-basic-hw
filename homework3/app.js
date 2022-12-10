1894f4afde9eed6c1a7ff273e1fcf86ef2002526
Kristijan <jankuloski.k@gmail.com>
2022-12-10 16:25:07 +0100

    Homework 3 finished

--- homework3/app.js
+++ homework3/app.js
@@ -1,5 +1,22 @@
-// Calculating birth year
-function calculateBirthYear(birthYear){
+// Temperature calculator
+function toFarenheit(temperature){
+    temperature = Number(temperature);
+    if(Number.isNaN(temperature)){
+        return "Invalid input";
+    }
+    return temperature * 1.8 + 32;
+}
+
+function toCelsius(temperature){
+    temperature = Number(temperature);
+    if(Number.isNaN(temperature)){
+        return "Invalid input";
+    }
+    return (5/9) * (temperature-32);
+}
+
+// Calculating age
+function calculateAge(birthYear){
     birthYear = Number(birthYear);
     if(Number.isNaN(birthYear)){
         return "Invalid year";
@@ -13,7 +30,7 @@ function calculateBirthYear(birthYear){
 
 // Variable type and return
 function returnTypeAndPrint(variable){
-    console.log(typeof(variable));
+    console.log(`Log form inside ${typeof(variable)}`);
     return typeof(variable);
 }
 
@@ -27,7 +44,7 @@ function calculateDogYears(years, dogOrHuman = "dog"){
     if(years === 0){
         return "0?";
     }
-    if(dogOrHuman === "dog"){
+    if(dogOrHuman === "dog" || dogOrHuman === "d"){
         if(years <= 1){
             return years*15;
         }
@@ -36,7 +53,7 @@ function calculateDogYears(years, dogOrHuman = "dog"){
         }
         return 24 + (years - 2) * 5;
     }
-    if(dogOrHuman === "human"){
+    if(dogOrHuman === "human" || dogOrHuman === "h"){
         if(years <= 15){
             return years/15;
         }
@@ -48,12 +65,43 @@ function calculateDogYears(years, dogOrHuman = "dog"){
     return "You didn't enter a valid dog/human";
 }
 
-// Birth year test
-// const birthYearInput = prompt("Enter your birth year");
-// console.log(calculateBirthYear(birthYearInput));
+// ATM
+function atmWithdraw(request){
+    if(request > accountBalance){
+        return "Not enough money";
+    }
+    accountBalance -= request;
+    return `${request} withdrawn / ${accountBalance} is new balcance`;
+}
 
+// ----- TESTS -----
+
+// convert faren ---
+const userTempC = prompt("Enter temperature in C");
+console.log(toFarenheit(userTempC));
+
+// convert celc ---
+const userTempF = prompt("Enter temperature in F");
+console.log(toCelsius(userTempF));
+
+// Birth year test ---
+const birthYearInput = prompt("Enter your birth year");
+console.log(calculateAge(birthYearInput));
+
+// Type test ---
 const nullObj = null;
 const numberObj = 12;
 const stringObj = "yes";
 const boolObj = true;
 let undefinedObj;
+console.log(`Form outside: ${returnTypeAndPrint(nullObj)}, ${returnTypeAndPrint(numberObj)}, ${returnTypeAndPrint(stringObj)}, ${returnTypeAndPrint(boolObj)}, ${returnTypeAndPrint(undefined)}`);
+
+// Dog year calculation test ---
+const yearInput = prompt("Enter age");
+const dogOrHuman = prompt("Enter to conver to dog or human years (dog/human) (d/h)");
+console.log(calculateDogYears(yearInput, dogOrHuman));
+
+// ATM test ---
+let accountBalance = 500;
+let requestedAmmount = prompt("Enter ammount you would like to withdraw");
+console.log(atmWithdraw(requestedAmmount));
